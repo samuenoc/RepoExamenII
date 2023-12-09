8cc2b8b9d4e67a0b5ddc439daa70fe027de3b7ff
samuenoc <sreyesz@unah.hn>
2023-12-08 22:02:45 -0600

    cambios

--- src/main/java/com/repo/repo/servicios/impl/PosicionServiceImpl.java
+++ src/main/java/com/repo/repo/servicios/impl/PosicionServiceImpl.java
@@ -1,11 +1,12 @@
 package com.repo.repo.servicios.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.repo.repo.modelos.Posicion;
 import com.repo.repo.repositorios.PosicionRepository;
 import com.repo.repo.servicios.PosicionService;
-
+@Service
 public class PosicionServiceImpl implements PosicionService {
     @Autowired PosicionRepository posicionRepository;
     @Override
